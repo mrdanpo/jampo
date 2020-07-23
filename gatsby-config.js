1d1b11cf69d9dc913bcc02c5acb4945a2ca6ada5
Daniel Proctor <daniel.proctor01@gmail.com>
2020-07-23 17:48:01 +0100

    setup netlify cms and markdown transform

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,22 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/src/markdown-pages`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/markdown-pages`,
+        name: `markdown-pages`,
+      },
+    },
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
---
+++ src/markdown-pages/index.md
@@ -0,0 +1,5 @@
+---
+slug: "/index.html"
+date: "2020-07-23"
+title: "Home"
+---
