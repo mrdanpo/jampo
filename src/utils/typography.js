db83706930b86d44737e5e009d14ea9ef243d1c9
Daniel Proctor <daniel.proctor01@gmail.com>
2020-07-31 14:43:49 +0100

    added gatsby typography plugin

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -14,12 +14,9 @@ delete Wordpress2016.googleFonts
 const typography = new Typography(Wordpress2016)
 
 // Hot reload typography in development.
-// if (process.env.NODE_ENV !== `production`) {
-//   typography.injectStyles()
-// }
-
-// typograhy not working in prod without this.
-typography.injectStyles()
+if (process.env.NODE_ENV !== `production`) {
+  typography.injectStyles()
+}
 
 export default typography
 export const rhythm = typography.rhythm
