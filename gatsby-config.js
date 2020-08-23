39e6249a5f2cc67ba0a32ef8a4799efe56f7cd5f
Daniel Proctor <daniel.proctor01@gmail.com>
2020-08-24 00:02:09 +0100

    fix path

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,7 +47,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/static/favicon.ico`, // This path is relative to the root of the site.
+        icon: `static/favicon.ico`, // This path is relative to the root of the site.
       },
     },
     {
