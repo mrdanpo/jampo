8b27f2e971a7c8b0f5f441780bae918e1be375b4
Daniel Proctor <daniel.proctor01@gmail.com>
2020-08-24 00:05:23 +0100

    use png for gatsy icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,7 +47,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `static/favicon.ico`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
     {
